02cbc4b3b7fdf8593c5528b53b262cdbafb04b82
GaidamakUA <jmazurevich@gmail.com>
2014-11-25 10:58:13 +0200

    Raw register activity xml

--- app/src/main/java/ua/com/studiovision/euromaidan/LoginActivity.java
+++ app/src/main/java/ua/com/studiovision/euromaidan/LoginActivity.java
@@ -2,9 +2,13 @@ package ua.com.studiovision.euromaidan;
 
 import android.app.Activity;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
 @EActivity (R.layout.activity_login)
 public class LoginActivity extends Activity {
-
+    @Click(R.id.register_button)
+    void register() {
+        RegisterActivity_.intent(this).start();
+    }
 }
---
+++ app/src/main/java/ua/com/studiovision/euromaidan/RegisterActivity.java
@@ -0,0 +1,10 @@
+package ua.com.studiovision.euromaidan;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_register)
+public class RegisterActivity extends Activity {
+
+}
