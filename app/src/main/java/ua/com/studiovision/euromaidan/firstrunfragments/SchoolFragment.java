91e64f3362aa4140040e8785317b51bfe53c25ae
JustWatermelon <kyrylchuk.yuriy@gmail.com>
2014-11-27 13:39:35 +0200

    Implementation of country auto-completion (read from DB)

--- app/src/main/java/ua/com/studiovision/euromaidan/firstrunfragments/SchoolFragment.java
+++ app/src/main/java/ua/com/studiovision/euromaidan/firstrunfragments/SchoolFragment.java
@@ -2,13 +2,16 @@ package ua.com.studiovision.euromaidan.firstrunfragments;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.database.Cursor;
 import android.widget.AutoCompleteTextView;
-
+import android.widget.FilterQueryProvider;
+import android.widget.SimpleCursorAdapter;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
-
 import ua.com.studiovision.euromaidan.R;
+import ua.com.studiovision.euromaidan.provider.country.CountryColumns;
+import ua.com.studiovision.euromaidan.provider.country.CountrySelection;
 
 @EFragment (R.layout.fragment_school)
 public class SchoolFragment extends Fragment {
@@ -20,11 +23,41 @@ public class SchoolFragment extends Fragment {
     AutoCompleteTextView schoolAutoCompleteTextView;
 
     FirstRunFragmentListener firstRunFragmentListener;
+    SimpleCursorAdapter countryCursorAdapter;
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         firstRunFragmentListener = (FirstRunFragmentListener) activity;
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        countryCursorAdapter = new SimpleCursorAdapter(getActivity().getBaseContext(), android.R.layout.simple_dropdown_item_1line, null,
+                new String[] { CountryColumns.COUNTRY_NAME },
+                new int[] {android.R.id.text1},
+                0);
+        countryAutoCompleteTextView.setAdapter(countryCursorAdapter);
+
+        countryCursorAdapter.setFilterQueryProvider(new FilterQueryProvider() {
+            public Cursor runQuery(CharSequence str) {
+                return getCursor(str);
+            } });
+
+        countryCursorAdapter.setCursorToStringConverter(new SimpleCursorAdapter.CursorToStringConverter() {
+            public CharSequence convertToString(Cursor cur) {
+                return cur.getString(cur.getColumnIndex(CountryColumns.COUNTRY_NAME));
+            }});
+    }
+
+    private Cursor getCursor(CharSequence charSequence){
+        CountrySelection countrySelection = new CountrySelection();
+        countrySelection.countryName(charSequence.toString());
+        return getActivity().getBaseContext().getContentResolver().query(CountryColumns.CONTENT_URI,CountryColumns.ALL_COLUMNS,
+                countrySelection.sel(), countrySelection.args(),null);
     }
 
     @Click(R.id.saveButton)
