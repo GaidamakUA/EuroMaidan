6aa4840e9389be084c4a45fcae5e4ab0c2aa0e6f
JustWatermelon <kyrylchuk.yuriy@gmail.com>
2014-11-29 11:55:43 +0200

    Refactored getting cursor in school fragment (add getCursor method). Implemented city auto-completion

--- app/src/main/java/ua/com/studiovision/euromaidan/firstrunfragments/SchoolFragment.java
+++ app/src/main/java/ua/com/studiovision/euromaidan/firstrunfragments/SchoolFragment.java
@@ -3,17 +3,18 @@ package ua.com.studiovision.euromaidan.firstrunfragments;
 import android.app.Activity;
 import android.app.Fragment;
 import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
 import android.widget.AutoCompleteTextView;
 import android.widget.CursorAdapter;
 import android.widget.FilterQueryProvider;
 import android.widget.SimpleCursorAdapter;
-
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
-
 import ua.com.studiovision.euromaidan.R;
+import ua.com.studiovision.euromaidan.provider.city.CityColumns;
+import ua.com.studiovision.euromaidan.provider.city.CityCursor;
 import ua.com.studiovision.euromaidan.provider.country.CountryColumns;
 import ua.com.studiovision.euromaidan.provider.country.CountryCursor;
 
@@ -29,25 +30,27 @@ public class SchoolFragment extends Fragment {
 
     FirstRunFragmentListener firstRunFragmentListener;
     SimpleCursorAdapter countryCursorAdapter;
+    SimpleCursorAdapter cityCursorAdapter;
 
-    private Long countryId = -1l;
+    private Long countryId = -1L;
+    private Long cityId = -1L;
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         firstRunFragmentListener = (FirstRunFragmentListener) activity;
-
-
     }
 
     @Override
     public void onStart() {
         super.onStart();
+
+        //---------------Country autoCompleteTextView adapter--------------------------//
         countryCursorAdapter = new SimpleCursorAdapter(getActivity().getBaseContext(),
                 android.R.layout.simple_dropdown_item_1line,
                 null,
-                new String[]{CountryColumns.COUNTRY_NAME},
-                new int[]{android.R.id.text1},
+                new String[] {CountryColumns.COUNTRY_NAME},
+                new int[] {android.R.id.text1},
                 CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
         countryAutoCompleteTextView.setAdapter(countryCursorAdapter);
 
@@ -56,22 +59,7 @@ public class SchoolFragment extends Fragment {
                 if (str == null || str.length() < 1)
                     return null;
                 firstRunFragmentListener.tryRequestCountries(str.toString());
-                StringBuilder stringBuilder = new StringBuilder();
-                stringBuilder.append(CountryColumns.COUNTRY_NAME);
-                stringBuilder.append(" LIKE '%");
-                stringBuilder.append(str.toString());
-                stringBuilder.append("%' OR ");
-                stringBuilder.append(CountryColumns.COUNTRY_NAME);
-                stringBuilder.append(" LIKE '%");
-                stringBuilder.append(Character.toUpperCase(str.charAt(0)));
-                stringBuilder.append(str.subSequence(1, str.length()));
-                stringBuilder.append("%'");
-                Log.v(TAG, "str=" + str + "; select=" + stringBuilder.toString());
-                Cursor cursor = getActivity().getBaseContext().getContentResolver()
-                        .query(CountryColumns.CONTENT_URI, CountryColumns.ALL_COLUMNS,
-                                stringBuilder.toString(), null, null);
-                Log.v(TAG, "Cursor=" + cursor.getCount());
-                return cursor;
+                return getCursor(str,CountryColumns.COUNTRY_NAME,CountryColumns.CONTENT_URI,CountryColumns.ALL_COLUMNS);
             }
         });
 
@@ -83,12 +71,69 @@ public class SchoolFragment extends Fragment {
                 return cursor.getCountryName();
             }
         });
+
+        //---------------City autoCompleteTextView adapter---------------------------//
+        cityCursorAdapter = new SimpleCursorAdapter(getActivity().getBaseContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                null,
+                new String[] {CityColumns.CITY_NAME},
+                new int[] {android.R.id.text1},
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        cityAutoCompleteTextView.setAdapter(cityCursorAdapter);
+
+        cityCursorAdapter.setFilterQueryProvider(new FilterQueryProvider() {
+            @Override
+            public Cursor runQuery(CharSequence str) {
+                if (str == null || str.length() <1 || countryId == -1L)
+                    return null;
+                Log.v(TAG,"Country id from city adapter:"+ countryId);
+                firstRunFragmentListener.tryRequestCities(str.toString(),countryId);
+                return getCursor(str,CityColumns.CITY_NAME,CityColumns.CONTENT_URI,CityColumns.ALL_COLUMNS);
+            }
+        });
+
+        cityCursorAdapter.setCursorToStringConverter(new SimpleCursorAdapter.CursorToStringConverter() {
+            @Override
+            public CharSequence convertToString(Cursor cur) {
+                CityCursor cursor = new CityCursor(cur);
+                cityId = cursor.getCityId();
+                return cursor.getCityName();
+            }
+        });
+
+    }
+
+    /**
+     * Get cursor from DB
+     * @param str User input
+     * @param columnName Column for selection
+     * @param columnsContentURI Uri to all columns
+     * @param projection Table projection for query
+     * @return  Cursor with selected data or empty cursor if data is not found
+     */
+    private Cursor getCursor(CharSequence str, String columnName, Uri columnsContentURI, String[] projection){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(columnName);
+        stringBuilder.append(" LIKE '%");
+        stringBuilder.append(str.toString());
+        stringBuilder.append("%' OR ");
+        stringBuilder.append(columnName);
+        stringBuilder.append(" LIKE '%");
+        stringBuilder.append(Character.toUpperCase(str.charAt(0)));
+        stringBuilder.append(str.subSequence(1, str.length()));
+        stringBuilder.append("%'");
+        Log.v(TAG, "str=" + str + "; select=" + stringBuilder.toString());
+        Cursor cursor = getActivity().getBaseContext().getContentResolver()
+                .query(columnsContentURI, projection,
+                        stringBuilder.toString(), null, null);
+        Log.v(TAG, "Cursor=" + cursor.getCount());
+        return cursor;
     }
 
     @Click(R.id.saveButton)
     void onSaveButtonClick() {
         // XXX test content
-//        firstRunFragmentListener.tryRequestCountries(countryAutoCompleteTextView.getText().toString());
+        //firstRunFragmentListener.tryRequestCountries(countryAutoCompleteTextView.getText().toString());
         Log.v(TAG, "CountryId=" + countryId);
         if (countryId == -1) {
             return;
